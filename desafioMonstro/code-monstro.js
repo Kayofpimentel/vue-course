52b597157159b938a8e219e87dd4d23a7869489b
Kayofpimentel <kayofpimentel@gmail.com>
2020-03-24 14:57:37 -0300

    Implementando a interface geral para o desafio do monstro. Implementando a interface dos grupos para o desafio do monstro.

--- desafioMonstro/code-monstro.js
+++ desafioMonstro/code-monstro.js
@@ -2,6 +2,14 @@ const jogo = new Vue({
 
     el: '#jogo-monstro',
     data:{
+        aliados:[
+            {classe: "Mago", vida: 120},
+            {classe: "Guerreiro", vida: 120}
+        ],
+        inimigos:[
+            {classe: "Goblin", vida: 50},
+            {classe: "Ogre", vida: 150}
+        ],
         
     },
     computed:{
