f449bb36f65bf23f8a5c8337644f320fa5287b3f
Kayofpimentel <kayofpimentel@gmail.com>
2020-05-28 21:14:22 -0300

    Conclusão do desafio do módulo 9, utilizando componentes dinâmicos e slots.

---
+++ componentesAvancado/comp-avancado-desafio/src/components/Nenhuma.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="caixa nenhuma">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    strong{
+        color: gray;
+    }
+</style>
