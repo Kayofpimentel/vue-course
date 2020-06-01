8f9629de2ab43c6f179e0965332428deb28f8d92
Kayofpimentel <kayofpimentel@gmail.com>
2020-06-01 12:52:03 -0300

    Criação do projeto do desafio TODO. Criação dos arquivos referentes a estrutura e estilo do app. Mudança da logo e configurações do estilo do body e do componente Header.

---
+++ desafiotodo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Header imgSrc="./assets/logo.png">
+    </Header>
+    <hr class="header-separator">
+    <Content msg="TODO APP" />
+    <Footer>
+      <img alt="TODO app logo" />
+    </FOoter>
+  </div>
+</template>
+
+<script>
+import Content from "./components/content.vue";
+import Header from "./components/header.vue"
+import Footer from "./components/footer.vue"
+
+export default {
+  name: "App",
+  components: {
+    Content, Header, Footer
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("./estilo.scss");
+
+#app {
+  padding: 0 0 0 0;
+  margin: 2vh 0 0 0;
+  width: 100vw;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.header-style{
+  img{
+    max-width: 12vh ;
+  }
+
+}
+</style>
---
+++ desafiotodo/src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header-style page-header">
+    <h1>Let's TODO</h1>
+    <img alt="TODO  app logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        imgSrc: String
+    }
+}
+</script>
+
+<style>
+
+</style>
