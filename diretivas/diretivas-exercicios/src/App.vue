218809f04c4038be5c2eb0d155614a798716073e
Kayofpimentel <kayofpimentel@gmail.com>
2020-07-03 12:08:51 -0300

    Criação do projeto para os exemplos do capítulo 12 de diretivas. Demonstração do uso das diretivas v-text e v-html. Demonstração do uso de uma diretiva personalizada com argumentos e modificadores. Demonstração do uso de uma diretiva criada no próprio componente.

---
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr>
+		<p v-text="'Usando diretiva v-text'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<p v-destaque="'darkblue'">Teste de diretiva</p>
+		<p v-destaque:fundo="corDeFundo"> Teste de diretiva com argumento </p>
+		<p v-destaque:fundo.atrasar="'darkgrey'"> Teste de diretiva com modificador </p>
+		<hr>
+		<p v-hideAndSeek="2000"> Teste de diretiva local </p>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			corDeFundo:"red"
+		}
+	},
+	directives:{
+		hideAndSeek(el, binding){
+			let atraso = binding.value
+			setInterval(() => {
+				if(el.style.visibility !== 'hidden'){
+					el.style.visibility = 'hidden'
+				}else{
+					el.style.visibility = 'inherit'
+				}
+			}, atraso);
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+p{
+	margin: 3vh auto; 
+}
+</style>
