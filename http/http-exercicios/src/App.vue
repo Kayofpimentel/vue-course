6fdbe627479935533e46cbb9f597fa66303fcba5
Kayofpimentel <kayofpimentel@gmail.com>
2020-07-15 18:37:47 -0300

    Criação do projeto para o módulo 15, de requisições http Criação de um banco de dados no Firebase para testes das requisições. Primeiro exemplo realizando uma requisição POST com dados de exemplo.

---
+++ http/http-exercicios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  created(){
+    // console.log("exec")
+    // this.$http.post("usuarios.json",{
+    //   nome: "Maria",
+    //   email: "maria_testehttp@gmail.com"
+    // }).then(res => console.log(res))
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
