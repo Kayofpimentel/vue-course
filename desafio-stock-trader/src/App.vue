fbc134bb0387c83f9680359c2c9d8d413ce7e32c
Kayofpimentel <kayofpimentel@gmail.com>
2020-10-28 12:42:06 -0300

    Criação e instalação do projeto StockTrader do módulo 18. Criação do esqueleto inicial dos componentes. Criação de um estilo default e substituição das imagens e ícones. Adição do Bootstrap no projeto.

---
+++ desafio-stock-trader/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="app">
+  <div class="nav">
+    <span class="logo">Stock Trader</span>
+    <button>Inicio</button>
+    <button>Inicio</button>
+    <button>Inicio</button>
+  </div>
+  <router-view/>
+</div>
+</template>
+
+<style lang="scss">
+
+@import url("./assets/bootstrap.min.css");
+@import url("./assets/default.css");
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.nav {
+  margin: 1rem auto 1.5rem 1rem;
+  text-align: left;
+  button {
+    font-weight: bold;
+    color: #2c3e50;
+    white-space: pre;
+  }
+  .logo{
+    margin-right: 2rem;
+  }
+}
+</style>
---
+++ desafio-stock-trader/src/components/Inicio.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="inicio">
+    <h1 class="badge">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#inicio{
+  text-align: left;
+}
+a {
+  color: #42b983;
+}
+</style>
